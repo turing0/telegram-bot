893da632841109b6bff98212d49c28d45b1d6db1
turing0 <turing175@gmail.com>
2023-05-12 23:06:41 +0800

    update

--- pages/api/telegram-bot.js
+++ pages/api/telegram-bot.js
@@ -5,13 +5,24 @@ import TelegramBot from 'telegram-bot-api'
 const myChatId = '5525041552'
 const token = '5635808285:AAGP1SilFciikrQd_SMr68J70OlVVJKiVsk'
 
-
-const bot = new TelegramBot(token, {webHook: {port: process.env.PORT}})
-
-bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, msg.text)
-})
-
-export default (req, res) => {
-  bot.webHookHandler(req, res) 
-}
+export default async (req, res) => {
+    const tgbot = '5635808285:AAGP1SilFciikrQd_SMr68J70OlVVJKiVsk';
+  
+    if (req.body.message.text === '/start') {
+      const message =
+        'Welcome to <i>NextJS News Channel</i> <b>' +
+        req.body.message.from.first_name +
+        '</b>.%0ATo get a list of commands sends /help';
+      const ret = await fetch(
+        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+      );
+    }
+    if (req.body.message.text === '/help') {
+      const message =
+        'Help for <i>NextJS News Channel</i>.%0AUse /search <i>keyword</i> to search for <i>keyword</i> in my Medium publication';
+      const ret = await fetch(
+        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+      );
+    }
+    res.status(200).send('OK');
+  };
