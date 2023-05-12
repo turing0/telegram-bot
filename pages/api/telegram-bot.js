cff3b548453fd3e099c1c8522c701c50308c6a39
turing0 <turing175@gmail.com>
2023-05-12 22:56:31 +0800

    update

--- pages/api/telegram-bot.js
+++ pages/api/telegram-bot.js
@@ -1,24 +1,30 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { TelegramBot } from 'node-telegram-bot-api';
+import { Telegram } from 'telegram-bot-api';
 
 const myChatId = '5525041552'
 const token = '5635808285:AAGP1SilFciikrQd_SMr68J70OlVVJKiVsk'
-const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, '你好！请向我发送消息~');
-});
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    const username = msg.from.username;
-    const forwardedMessage = `Message from user ${chatId}: ${msg.text}\n@${username}`;
-
-    bot.sendMessage(chatId, '您的反馈已记录！我们将尽快回复您！');
-    bot.sendMessage(myChatId, forwardedMessage);
-});
-
-export default (req, res) => {
+const api = new Telegram({
+    token: token,
+  });
+  
+  api.on('message', (message) => {
+    const chatId = message.chat.id;
+    const username = message.from.username;
+    const forwardedMessage = `Message from user ${chatId}: ${message.text}\n@${username}`;
+    
+    api.sendMessage({
+      chat_id: chatId,
+      text: '您的反馈已记录！我们将尽快回复您！'
+    });
+  
+    api.sendMessage({
+      chat_id: myChatId,
+      text: forwardedMessage,
+    });
+  });
+  
+  export default (req, res) => {
     res.status(200).json({ status: 'Bot is running...' });
   };
