de109b58e4c74ecad827af5111ebd8a8ce88a2b4
turing0 <turing175@gmail.com>
2023-05-12 23:00:19 +0800

    update

--- pages/api/telegram-bot.js
+++ pages/api/telegram-bot.js
@@ -1,30 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Telegram } from 'telegram-bot-api';
+import TelegramBot from 'telegram-bot-api'
 
 const myChatId = '5525041552'
 const token = '5635808285:AAGP1SilFciikrQd_SMr68J70OlVVJKiVsk'
 
 
-const api = new Telegram({
-    token: token,
-  });
-  
-  api.on('message', (message) => {
-    const chatId = message.chat.id;
-    const username = message.from.username;
-    const forwardedMessage = `Message from user ${chatId}: ${message.text}\n@${username}`;
-    
-    api.sendMessage({
-      chat_id: chatId,
-      text: '您的反馈已记录！我们将尽快回复您！'
-    });
-  
-    api.sendMessage({
-      chat_id: myChatId,
-      text: forwardedMessage,
-    });
-  });
-  
-  export default (req, res) => {
-    res.status(200).json({ status: 'Bot is running...' });
-  };
+const bot = new TelegramBot(token, {webHook: {port: process.env.PORT}})
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, msg.text)
+})
+
+export default (req, res) => {
+  bot.webHookHandler(req, res) 
+}
