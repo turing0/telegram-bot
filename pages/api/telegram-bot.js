032fee0513fd3cd4f4c48fdbc8a529a25c3cea14
turing0 <turing175@gmail.com>
2023-05-12 23:29:02 +0800

    update

--- pages/api/telegram-bot.js
+++ pages/api/telegram-bot.js
@@ -1,28 +1,57 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { Telegram } from 'telegram-bot-api';
-import TelegramBot from 'telegram-bot-api'
 
 const myChatId = '5525041552'
 const token = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk'
 
-export default async (req, res) => {
-    const tgbot = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk';
+const TelegramBot = require('node-telegram-bot-api');
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
+
+
+
+
+// export default async (req, res) => {
+//     const tgbot = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk';
   
-    if (req.body.message.text === '/start') {
-      const message =
-        'Welcome to <i>NextJS News Channel</i> <b>' +
-        req.body.message.from.first_name +
-        '</b>.%0ATo get a list of commands sends /help';
-      const ret = await fetch(
-        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
-      );
-    }
-    if (req.body.message.text === '/help') {
-      const message =
-        'Help for <i>NextJS News Channel</i>.%0AUse /search <i>keyword</i> to search for <i>keyword</i> in my Medium publication';
-      const ret = await fetch(
-        `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
-      );
-    }
-    res.status(200).send('OK');
-  };
+//     if (req.body.message.text === '/start') {
+//       const message =
+//         'Welcome to <i>NextJS News Channel</i> <b>' +
+//         req.body.message.from.first_name +
+//         '</b>.%0ATo get a list of commands sends /help';
+//       const ret = await fetch(
+//         `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+//       );
+//     }
+//     if (req.body.message.text === '/help') {
+//       const message =
+//         'Help for <i>NextJS News Channel</i>.%0AUse /search <i>keyword</i> to search for <i>keyword</i> in my Medium publication';
+//       const ret = await fetch(
+//         `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+//       );
+//     }
+//     res.status(200).send('OK');
+//   };
