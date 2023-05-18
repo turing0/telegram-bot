83128b8a92ece70d1d1032de41c3eb1fe1405c6b
turing0 <turing175@gmail.com>
2023-05-18 23:46:12 +0800

    update

--- pages/api/telegram-bot.js
+++ pages/api/telegram-bot.js
@@ -6,7 +6,6 @@ const token = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk'
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { message } = req.body;
-    const match = message.text.match(/^Message from user (\d+):/);
 
     if (message.text === '/start') {
       const msg =
@@ -17,28 +16,30 @@ export default async function handler(req, res) {
         `https://api.telegram.org/bot${token}/sendMessage?chat_id=${message.chat.id}&text=${msg}&parse_mode=HTML`
       );
     }
-    else if (message.reply_to_message && match) {  // Forward the message
-      const chatId = parseInt(match[1]);
-      const replyMessagePromise = fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          chat_id: chatId,
-          text: '回复成功',
-        }),
-      });
-
-      const okMessagePromise = fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          chat_id: message.chat.id,
-          text: '回复成功',
-        }),
-      });
-
-      await Promise.all([replyMessagePromise, okMessagePromise]);
-
+    else if (message.reply_to_message) {  // Forward the message
+      const match = message.reply_to_message.text.match(/^Message from user (\d+):/);
+      if (match) {
+        const chatId = parseInt(match[1]);
+        const replyMessagePromise = fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            chat_id: chatId,
+            text: '回复成功',
+          }),
+        });
+  
+        const okMessagePromise = fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            chat_id: message.chat.id,
+            text: '回复成功',
+          }),
+        });
+  
+        await Promise.all([replyMessagePromise, okMessagePromise]);
+      }
     }
     else if (message && message.text) {
       // Send the user's message back to them
