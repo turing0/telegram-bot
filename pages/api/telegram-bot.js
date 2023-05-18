6111c3faab6fc8409654609047d99120205f38d8
turing0 <turing175@gmail.com>
2023-05-18 21:32:11 +0800

    update

--- pages/api/telegram-bot.js
+++ pages/api/telegram-bot.js
@@ -1,67 +1,69 @@
-import { Telegram } from 'telegram-bot-api';
+// import { Telegram } from 'telegram-bot-api';
 
 const myChatId = '5525041552'
 const token = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk'
+const TelegramBot = require('node-telegram-bot-api');
 
-export default async (req, res) => {
-  const tgbot = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk';
+// export default async (req, res) => {
+//   const tgbot = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk';
 
-  if (req.body.message.text === '/start') {
-    const message =
-      'Welcome to <i>NextJS News Channel</i> <b>' +
-      req.body.message.from.first_name +
-      '</b>.%0ATo get a list of commands sends /help';
-    const ret = await fetch(
-      `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
-    );
-  }
-  if (req.body.message.text === '/help') {
-    const message =
-      'Help for <i>NextJS News Channel</i>.%0AUse /search <i>keyword</i> to search for <i>keyword</i> in my Medium publication';
-    const ret = await fetch(
-      `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
-    );
-  }
-  else {
-    const message = req.body.message.text;
-    const ret = await fetch(
-      `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
-    );
-  }
-  res.status(200).send('OK');
-};
+//   if (req.body.message.text === '/start') {
+//     const message =
+//       'Welcome to <i>NextJS News Channel</i> <b>' +
+//       req.body.message.from.first_name +
+//       '</b>.%0ATo get a list of commands sends /help';
+//     const ret = await fetch(
+//       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+//     );
+//   }
+//   if (req.body.message.text === '/help') {
+//     const message =
+//       'Help for <i>NextJS News Channel</i>.%0AUse /search <i>keyword</i> to search for <i>keyword</i> in my Medium publication';
+//     const ret = await fetch(
+//       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+//     );
+//   }
+//   else {
+//     const message = req.body.message.text;
+//     const ret = await fetch(
+//       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+//     );
+//   }
+//   res.status(200).send('OK');
+// };
 
-// // Create a bot that uses 'polling' to fetch new updates
-// const bot = new TelegramBot(token, {polling: true});
 
-// // Matches "/echo [whatever]"
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//   // 'msg' is the received Message from Telegram
-//   // 'match' is the result of executing the regexp above on the text content
-//   // of the message
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
 
-//   const chatId = msg.chat.id;
-//   const resp = match[1]; // the captured "whatever"
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
 
-//   // send back the matched "whatever" to the chat
-//   bot.sendMessage(chatId, resp);
-// });
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
 
-// // Listen for any kind of message. There are different kinds of
-// // messages.
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id;
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
 
-//   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(chatId, 'Received your message');
-// });
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
 
-// // 监听任意消息类型
-// bot.onAnyMessage((msg) => {
-// const chatId = msg.chat.id;
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
 
-// console.log(msg)  // 打印收到的消息,方便调试  
+// 监听任意消息类型
+bot.onAnyMessage((msg) => {
+const chatId = msg.chat.id;
 
-// // 发送确认消息
-// bot.sendMessage(chatId, 'Received your message');  
-// });
+console.log(msg)  // 打印收到的消息,方便调试  
+
+// 发送确认消息
+bot.sendMessage(chatId, 'Received your message');  
+});
