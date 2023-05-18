5e9b3299f71ef7728b79e38e008c7269b1ceae7f
turing0 <turing175@gmail.com>
2023-05-18 21:57:38 +0800

    update

--- pages/api/telegram-bot.js
+++ pages/api/telegram-bot.js
@@ -4,37 +4,13 @@ const myChatId = '5525041552'
 const token = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk'
 const TelegramBot = require('node-telegram-bot-api');
 
-// export default async (req, res) => {
-//   const tgbot = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk';
-
-//   if (req.body.message.text === '/start') {
-//     const message =
-//       'Welcome to <i>NextJS News Channel</i> <b>' +
-//       req.body.message.from.first_name +
-//       '</b>.%0ATo get a list of commands sends /help';
-//     const ret = await fetch(
-//       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
-//     );
-//   }
-//   if (req.body.message.text === '/help') {
-//     const message =
-//       'Help for <i>NextJS News Channel</i>.%0AUse /search <i>keyword</i> to search for <i>keyword</i> in my Medium publication';
-//     const ret = await fetch(
-//       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
-//     );
-//   }
-//   else {
-//     const message = req.body.message.text;
-//     const ret = await fetch(
-//       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
-//     );
-//   }
-//   res.status(200).send('OK');
-// };
-
-
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+// const bot = new TelegramBot(token, {polling: true});
+// Create a bot that uses 'webhook' to fetch new updates
+const bot = new TelegramBot(token, {webHook: {port: process.env.PORT, host: process.env.HOST}});
+
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${process.env.URL}/bot${token}`);
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -65,3 +41,32 @@ bot.on('message', (msg) => {
 //   // 发送确认消息
 //   bot.sendMessage(chatId, 'Received your message');  
 // });
+
+
+// export default async (req, res) => {
+//   const tgbot = '5975588613:AAFlmhxm_XRZ4RhqLOnfK7StJVbkJ7fINZk';
+
+//   if (req.body.message.text === '/start') {
+//     const message =
+//       'Welcome to <i>NextJS News Channel</i> <b>' +
+//       req.body.message.from.first_name +
+//       '</b>.%0ATo get a list of commands sends /help';
+//     const ret = await fetch(
+//       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+//     );
+//   }
+//   if (req.body.message.text === '/help') {
+//     const message =
+//       'Help for <i>NextJS News Channel</i>.%0AUse /search <i>keyword</i> to search for <i>keyword</i> in my Medium publication';
+//     const ret = await fetch(
+//       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+//     );
+//   }
+//   else {
+//     const message = req.body.message.text;
+//     const ret = await fetch(
+//       `https://api.telegram.org/bot${tgbot}/sendMessage?chat_id=${req.body.message.chat.id}&text=${message}&parse_mode=HTML`
+//     );
+//   }
+//   res.status(200).send('OK');
+// };
