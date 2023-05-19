912ed0f3fa10ab14d00793e71b1629bfc52a49b5
turing0 <turing175@gmail.com>
2023-05-19 15:04:30 +0800

    update

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -22,13 +22,19 @@ const WithStaticProps = ({ items }: Props) => (
       <Link href="/">Go home</Link>
     </p>
   </Layout>
+
 )
 
 export const getStaticProps: GetStaticProps = async () => {
   // Example for including static props in a Next.js function component page.
   // Don't forget to include the respective types for any props passed into
   // the component.
-  const items: User[] = sampleUserData
+  // const items: User[] = sampleUserData
+
+  const res = await fetch(`http://${process.env.VERCEL_URL}/api/users`)
+  const apiItems: User[] = await res.json()
+  const items: User[] = [...sampleUserData, ...apiItems]
+
   return { props: { items } }
 }
 
