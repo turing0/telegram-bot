64fc23cb59500a4d3c497da718a0c70bab0e2746
turing0 <turing175@gmail.com>
2023-05-19 14:24:29 +0800

    update

--- pages/api/users/save.ts
+++ pages/api/users/save.ts
@@ -7,7 +7,21 @@ const handler = (_req: NextApiRequest, res: NextApiResponse) => {
       throw new Error('Cannot find user data')
     }
 
-    res.status(200).json(sampleUserData)
+    // Assume you're getting the user data in the request body
+    const { id, ...data } = _req.body
+
+    // Find the user in the array
+    const userIndex = sampleUserData.findIndex(user => user.id === id)
+
+    if (userIndex === -1) {
+      throw new Error('User not found')
+    }
+
+    // Update the user data
+    sampleUserData[userIndex] = { ...sampleUserData[userIndex], ...data }
+
+    res.status(200).json(sampleUserData[userIndex])
+
   } catch (err: any) {
     res.status(500).json({ statusCode: 500, message: err.message })
   }
