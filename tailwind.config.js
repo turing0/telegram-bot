9d4a72ac31f0a7d6e103c18d1fa3ebede22b3621
turing0 <turing175@gmail.com>
2026-05-06 18:23:39 +0800

    feat: add main application component and configure Tailwind CSS content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}', 
     './interfaces/**/*.{js,ts,jsx,tsx}', 
     './components/**/*.{js,ts,jsx,tsx}'
